d7871f71cc6dfc5f7a996ad15cc02b0e2c8c3ea9
Piotr Zboś <piotr.zbos@o2.pl>
2024-07-24 16:02:26 +0200

    poprawka widoku teamow

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,9 +1,9 @@
 Geocoder.configure(
   lookup: :geoapify,
-  api_key: 'bef1801d9eaf4fa694fcc367a56e31ed', # Wstaw swój klucz API z Geoapify
-  timeout: 10, # Czas oczekiwania na odpowiedź
-  use_https: true, # Używaj HTTPS dla bezpieczniejszych połączeń
-  language: :en, # Ustaw język na angielski, można zmienić na inne obsługiwane języki
+  api_key: 'bef1801d9eaf4fa694fcc367a56e31ed', 
+  timeout: 10, 
+  use_https: true, 
+  language: :en, 
   # Opcjonalnie: logowanie
   logger: Rails.logger
 )
